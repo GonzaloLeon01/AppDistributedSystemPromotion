69a4d390167ee368db7c0ffef1349fab9c38c9be
Agustin-Perea <agus25050@hotmail.com>
2024-11-09 21:20:40 -0300

    Test para mqtt

--- backend/src/Test/mqttTest.js
+++ backend/src/Test/mqttTest.js
@@ -3,72 +3,56 @@
 const mqtt = require('mqtt');
 
 const brokerUrl = process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883';
-const topic = 'checkpoints';
+const topic = "checkpoint";
 const options = {
-    host:'localhost',
+    host: process.env.MQTTHOST,
+    port: process.env.MQTTPORT,
+    clientId: 'Backend',
+    username: "api",
+    password: "api",
+};
+const fallbackOptions = {
+    host: 'test.mosquitto.org',  // Servidor de prueba de Mosquitto
     port: 1883,
-    username: 'admin',
-    password: 'admin'
-  };
-const client = mqtt.connect(options);
+};
+const client = mqtt.connect(fallbackOptions);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 const startMockedMqttPublishers = () => {
-  setInterval(sendRandomData, 30000);
+    setInterval(sendRandomData, getRandomInt(1000, 20000));
 }
 
-function generateRandomCheckpoint() {
-    const id = Math.random()*29819;
-    const descriptions = ['checkpoint zone 1', 'checkpoint zone 2', 'checkpoint zone 3', 'checkpoint zone 4', 'checkpoint zone 5'];
-    const description = descriptions[Math.floor(Math.random() * descriptions.length)];
-    const lat = Math.random() * 10;
-    const long = Math.random() * 10;
-    return {
-        id: id,
-        lat: lat,
-        long: long,
-        description: description
-    };
-}
 
-function generateRandomAnimal() {
-    const id = Math.floor(Math.random()*10) ;
-    const descriptions = ['Animal', 'Animal 2', 'Animal 3', 'Animal 4', 'NOT Animal O.O'];
-    const description = descriptions[Math.floor(Math.random() * descriptions.length)];
-    const names = ['Juan', 'Mandarina', 'ElDiablo', 'Zuko', 'Escubi'];
-    const name = names[Math.floor(Math.random() * names.length)];
+function generateRandomMessage(i) {
     return {
-        id: id,
-        name: name,
-        description: description
-    };
-}
-const mockMessage = {
-    checkpointID: '1234',
-    animals: [
-        { id: '11:5e:e7:84:c4:f6', rssi: -50 },
-        { id: '7c:0a:3f:83:db:93', rssi: -62 },
-        { id: 'c2:5a:3d:ae:10:28', rssi: -73 }
-    ]
-};
-function generateRandomMessage(){
-    return {
-        checkpointID: '1234',
+        checkpointID: `8C:AA:B5:8B:F8:4${i}`,
         animals: [
-            { id: '11:5e:e7:84:c4:f6', rssi: Math.random()*-10 },
-            { id: '7c:0a:3f:83:db:93', rssi: Math.random()*-10 },
-            { id: 'c2:5a:3d:ae:10:28', rssi: Math.random()*-10 }
+            { id: '11:5e:e7:84:c4:f6', rssi: Math.random() * -20 },
+            { id: '7c:0a:3f:83:db:93', rssi: Math.random() * -20 },
+            { id: 'c2:5a:3d:ae:10:28', rssi: Math.random() * -20 },
+            { id: '0f:ad:28:07:10:14', rssi: Math.random() * -20 },
+            { id: '02:b3:ec:c2:17:72', rssi: Math.random() * -20 }
         ]
     };
 }
+
 function sendRandomData() {
-    const data = generateRandomMessage();
-    const message = JSON.stringify(data);
-    client.publish(topic, message);
-    console.log('Mensaje enviado:', message);
+
+    for(let i=0;i<4;i++){
+        const data = generateRandomMessage(i);
+        const message = JSON.stringify(data);
+        client.publish(topic, message);
+        console.log('Mensaje enviado:', message);
+    }
+
+    
 }
 
 client.on('connect', function () {
-    console.log('Conectado al broker correctamente. en:' + brokerUrl);
+    console.log('Conectado al broker correctamente. en:' + options);
 });
 
 client.on('error', function (error) {
